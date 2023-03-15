ffb9573dca55c454b5b46a655da477da6ce9c977
nko <solowingrazgriz@outlook.com>
2023-03-15 21:00:38 +0900

    Implement dynamic sketch area scaling

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ var canvas = document.querySelector("#sketch-sect");
 var pieceColor = "#D04343";
 var canvasColor = "#F0F8FF";
 var canvasPieces = new Array();
-//buttons
-var btnClear = document.querySelector("#btn-clear");
+var canvasSize = 5;
+//buttons and inputs
+var btnClear = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
+var sketchScale = document.querySelector("#sketch-scale");
 function retrieveColor(color) {
     if (color === void 0) { color = pieceColor; }
     return color;
 }
+
 function setCanvas(num) {
-    if (num === void 0) { num = 25; }
-    for (var i = 0; i <= num; i++) {
+    if (num === void 0) { num = canvasSize; }
+    if (canvas != null) {
+        canvas.style.gridTemplateColumns = "repeat(".concat(num, ", 1fr)");
+        canvas.style.gridTemplateRows = "repeat(".concat(num, ", 1fr)");
+    }
+    for (var i = 0; i < (num * num); i++) {
         canvasPieces.push(document.createElement("div"));
     }
     canvasPieces.forEach(function (element) {
@@ -27,5 +34,25 @@ function clearCanvas() {
         element.style["background-color"] = canvasColor;
     });
 }
+function resizeLabel() {
+    clearCanvas();
+    var sizeString = sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.value;
+    canvasSize = Number(sizeString);
+    var scaleLabel = document.getElementById("lbl-sketch-scale");
+    if (scaleLabel != null) {
+        scaleLabel.textContent = "Size: ".concat(sizeString, " x ").concat(sizeString);
+    }
+}
+function resizeSketchArea() {
+    canvasPieces.forEach(function (element) {
+        canvas === null || canvas === void 0 ? void 0 : canvas.removeChild(element);
+    });
+    for (var i = 0; i != canvasPieces.length;) {
+        canvasPieces.shift();
+    }
+    setCanvas(canvasSize);
+}
 setCanvas();
 btnClear === null || btnClear === void 0 ? void 0 : btnClear.addEventListener("click", clearCanvas);
+sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.addEventListener("input", resizeLabel);
+sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.addEventListener("change", resizeSketchArea);
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
 const numOfPieces:number = 5*5;
-const canvas = document.querySelector("#sketch-sect");
+const canvas:HTMLElement|null = document.querySelector("#sketch-sect");
 let pieceColor = "#D04343";
 let canvasColor = "#F0F8FF";
 let canvasPieces:HTMLElement[] = new Array();
+let canvasSize:number = 5;
 
-//buttons
-const btnClear = document.querySelector("#btn-clear");
+//buttons and inputs
+const btnClear:HTMLElement|null = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
+const sketchScale:HTMLInputElement|null = document.querySelector("#sketch-scale");
 
-function retrieveColor(color = pieceColor):string {
+function retrieveColor(color = pieceColor):string 
+{
 	return color;
 }
 
-function setCanvas(num = 25):void 
+function changeColor():void
 {
-	for (let i = 0; i <= num; i++) {
+
+}
+
+function setCanvas(num = canvasSize):void 
+{
+	if(canvas != null) {
+		canvas.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+		canvas.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+	}
+	for (let i = 0; i < (num*num); i++) {
 		canvasPieces.push(document.createElement("div"));
 	}
 	canvasPieces.forEach(element => {
@@ -32,6 +44,30 @@ function clearCanvas():void
 	})
 }
 
+function resizeLabel():void {
+	clearCanvas();
+	const sizeString:string|undefined = sketchScale?.value;
+	canvasSize = Number(sizeString);
+	let scaleLabel = document.getElementById("lbl-sketch-scale");
+	if (scaleLabel != null) {
+		scaleLabel.textContent = `Size: ${sizeString} x ${sizeString}`;
+	}
+}
+
+function resizeSketchArea()
+{
+	canvasPieces.forEach(element => {
+		canvas?.removeChild(element);
+	})
+	for (let i = 0; i != canvasPieces.length;) {
+		canvasPieces.shift();
+	}
+	setCanvas(canvasSize);
+}
+
+
 setCanvas();
 
 btnClear?.addEventListener("click", clearCanvas);
+sketchScale?.addEventListener("input", resizeLabel);
+sketchScale?.addEventListener("change", resizeSketchArea);
