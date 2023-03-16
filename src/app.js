2ac20d6779e26cdcc3677b7467c109d8ccf939ba
nko <solowingrazgriz@outlook.com>
2023-03-16 10:24:48 +0900

    Implement color picker

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 var numOfPieces = 5 * 5;
 var canvas = document.querySelector("#sketch-sect");
-var pieceColor = "#D04343";
+//buttons and inputs
+var btnPieceColor = document.querySelector("#btn-color");
+var btnClear = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
+var sketchScale = document.querySelector("#sketch-scale");
+var pieceColor = btnPieceColor === null || btnPieceColor === void 0 ? void 0 : btnPieceColor.value;
 var canvasColor = "#F0F8FF";
 var canvasPieces = new Array();
 var canvasSize = 5;
-//buttons and inputs
-var btnClear = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
-var sketchScale = document.querySelector("#sketch-scale");
-function retrieveColor(color) {
-    if (color === void 0) { color = pieceColor; }
-    return color;
+function changePieceColor() {
+    pieceColor = btnPieceColor === null || btnPieceColor === void 0 ? void 0 : btnPieceColor.value;
 }
 //meh.  i need to rethink this approach.
 function drawPieces(item) {
-    item.style.backgroundColor = pieceColor;
+    if (pieceColor != undefined) {
+        item.style.backgroundColor = pieceColor;
+    }
+    ;
     /* canvasPieces.forEach(element => {
         element.addEventListener("mouseover", function () {
             element.style.backgroundColor = pieceColor;
@@ -29,12 +32,13 @@ function setCanvas(num) {
     for (var i = 0; i < (num * num); i++) {
         canvasPieces.push(document.createElement("div"));
     }
-    /* canvasPieces.forEach(element => {
+    canvasPieces.forEach(function (element) {
         element.className = "gridPiece";
-        canvas?.appendChild(element);
-        element.addEventListener("click", function() {
-            drawPieces(element)});
-    }); */
+        canvas === null || canvas === void 0 ? void 0 : canvas.appendChild(element);
+        element.addEventListener("click", function () {
+            drawPieces(element);
+        });
+    });
 }
 function clearCanvas() {
     canvasPieces.forEach(function (element) {
@@ -60,6 +64,7 @@ function resizeSketchArea() {
     setCanvas(canvasSize);
 }
 setCanvas();
+btnPieceColor === null || btnPieceColor === void 0 ? void 0 : btnPieceColor.addEventListener("change", changePieceColor);
 btnClear === null || btnClear === void 0 ? void 0 : btnClear.addEventListener("click", clearCanvas);
 sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.addEventListener("input", resizeLabel);
 sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.addEventListener("change", resizeSketchArea); //the reason why I went with the change event instead of input was for performance concerns. I would guess it's a reasonably simple matter to change some text, but constantly resizing the grid might be more taxing, and it visually makes no difference either way.
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
 const numOfPieces:number = 5*5;
 const canvas:HTMLElement|null = document.querySelector("#sketch-sect");
-let pieceColor = "#D04343";
+
+
+//buttons and inputs
+const btnPieceColor:HTMLInputElement|null = document.querySelector("#btn-color");
+const btnClear:HTMLElement|null = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
+const sketchScale:HTMLInputElement|null = document.querySelector("#sketch-scale");
+let pieceColor:string|undefined = btnPieceColor?.value;
+
 let canvasColor = "#F0F8FF";
 let canvasPieces:HTMLElement[] = new Array();
 let canvasSize:number = 5;
 
-//buttons and inputs
-const btnClear:HTMLElement|null = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
-const sketchScale:HTMLInputElement|null = document.querySelector("#sketch-scale");
-
-function retrieveColor(color = pieceColor):string 
+function changePieceColor():void
 {
-	return color;
+	pieceColor = btnPieceColor?.value;
 }
 
 //meh.  i need to rethink this approach.
 function drawPieces(item:HTMLElement) 
 {
-	item.style.backgroundColor = pieceColor;
+	if (pieceColor != undefined) {
+		item.style.backgroundColor = pieceColor;
+	};
 	/* canvasPieces.forEach(element => {
 		element.addEventListener("mouseover", function () {
 			element.style.backgroundColor = pieceColor;
@@ -34,12 +39,12 @@ function setCanvas(num = canvasSize):void
 	for (let i = 0; i < (num*num); i++) {
 		canvasPieces.push(document.createElement("div"));
 	}
-	/* canvasPieces.forEach(element => {
+	canvasPieces.forEach(element => {
 		element.className = "gridPiece";
 		canvas?.appendChild(element);
 		element.addEventListener("click", function() {
 			drawPieces(element)});
-	}); */
+	});
 }
 
 function clearCanvas():void
@@ -73,6 +78,7 @@ function resizeSketchArea()
 
 setCanvas();
 
+btnPieceColor?.addEventListener("change", changePieceColor)
 btnClear?.addEventListener("click", clearCanvas);
 sketchScale?.addEventListener("input", resizeLabel);
 sketchScale?.addEventListener("change", resizeSketchArea);//the reason why I went with the change event instead of input was for performance concerns. I would guess it's a reasonably simple matter to change some text, but constantly resizing the grid might be more taxing, and it visually makes no difference either way.
