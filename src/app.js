85fff06d9745fa4bec6e23076ccad4721386d02c
nko <solowingrazgriz@outlook.com>
2023-03-16 12:21:02 +0900

    Implement canvas color picker

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ var numOfPieces = 5 * 5;
 var canvas = document.querySelector("#sketch-sect");
 //buttons and inputs
 var btnPieceColor = document.querySelector("#btn-color");
+var btnCanvasColor = document.querySelector("#btn-canvas-color");
 var btnClear = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
 var sketchScale = document.querySelector("#sketch-scale");
 var pieceColor = btnPieceColor === null || btnPieceColor === void 0 ? void 0 : btnPieceColor.value;
-var canvasColor = "#F0F8FF";
+var canvasColor = btnCanvasColor === null || btnCanvasColor === void 0 ? void 0 : btnCanvasColor.value;
 var canvasPieces = new Array();
 var canvasSize = 5;
 function changePieceColor() {
     pieceColor = btnPieceColor === null || btnPieceColor === void 0 ? void 0 : btnPieceColor.value;
+}
+function changeCanvasColor() {
+    canvasColor = btnCanvasColor === null || btnCanvasColor === void 0 ? void 0 : btnCanvasColor.value;
+    if (canvas != null) {
+        canvas.style["background-color"] = canvasColor;
+    }
 }
 function drawPieces(item) {
     if (pieceColor != undefined) {
@@ -64,6 +71,7 @@ function resizeSketchArea() {
 }
 setCanvas();
 btnPieceColor === null || btnPieceColor === void 0 ? void 0 : btnPieceColor.addEventListener("input", changePieceColor);
+btnCanvasColor === null || btnCanvasColor === void 0 ? void 0 : btnCanvasColor.addEventListener("input", changeCanvasColor);
 btnClear === null || btnClear === void 0 ? void 0 : btnClear.addEventListener("click", clearCanvas);
 sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.addEventListener("input", resizeLabel);
 sketchScale === null || sketchScale === void 0 ? void 0 : sketchScale.addEventListener("change", resizeSketchArea); //the reason why I went with the change event instead of input was for performance concerns. I would guess it's a reasonably simple matter to change some text, but constantly resizing the grid might be more taxing, and it visually makes no difference either way.
--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,27 @@ const canvas:HTMLElement|null = document.querySelector("#sketch-sect");
 
 //buttons and inputs
 const btnPieceColor:HTMLInputElement|null = document.querySelector("#btn-color");
+const btnCanvasColor:HTMLInputElement|null = document.querySelector("#btn-canvas-color");
 const btnClear:HTMLElement|null = document.querySelector("#btn-clear"); //TypeScript requires you to declare all technically possible types. Even if you know that the page is set up to only deliver HTMLElements, type safety dictates that this must also be declared potentially null, in the event that the query selector somehow fails to retrieve the element.
 const sketchScale:HTMLInputElement|null = document.querySelector("#sketch-scale");
 let pieceColor:string|undefined = btnPieceColor?.value;
+let canvasColor:string|undefined = btnCanvasColor?.value;
 
-let canvasColor = "#F0F8FF";
 let canvasPieces:HTMLElement[] = new Array();
 let canvasSize:number = 5;
 
 function changePieceColor():void
 {
 	pieceColor = btnPieceColor?.value;
+}
+
+function changeCanvasColor():void
+{
+	canvasColor = btnCanvasColor?.value;
+	if (canvas != null)
+	{
+		canvas.style["background-color"] = canvasColor;	
+	}
 }
 
 function drawPieces(item:HTMLElement) 
@@ -77,6 +87,7 @@ function resizeSketchArea()
 setCanvas();
 
 btnPieceColor?.addEventListener("input", changePieceColor);
+btnCanvasColor?.addEventListener("input", changeCanvasColor);
 btnClear?.addEventListener("click", clearCanvas);
 sketchScale?.addEventListener("input", resizeLabel);
 sketchScale?.addEventListener("change", resizeSketchArea);//the reason why I went with the change event instead of input was for performance concerns. I would guess it's a reasonably simple matter to change some text, but constantly resizing the grid might be more taxing, and it visually makes no difference either way.
